d473a4b27b25c2d915ee3d2db782da6291299f31
Raju Ahmmed <ahraju2003@gmail.com>
2025-05-24 12:35:00 +0600

    navbar with login and logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import {
+  BookOpenIcon,
+  CreditCard,
+  GraduationCap,
+  LogIn,
+  LogOutIcon,
+  ZapIcon,
+} from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex items-center justify-between py-4 px-6 bg-background border-b">
+      <Link
+        href={"/"}
+        className="text-xl font-extrabold text-primary flex items-center gap-2"
+      >
+        MasterClass <GraduationCap className="size-6" />
+      </Link>
 
-export default Navbar
+      <div className="flex items-center space-x-1 sm:space-x-4">
+        <Link
+          href={"/courses"}
+          className="flex items-center gap-1 px-3 py-2 rounded-md text-sm text-muted-foreground hover:text-primary hover:bg-secondary transition-colors"
+        >
+          <BookOpenIcon className="size-4" />
+          <span className="hidden sm:inline">Courses</span>
+        </Link>
+
+        <Link
+          href={"/pro"}
+          className="flex items-center gap-1 px-3 py-2 rounded-md text-sm text-muted-foreground hover:text-primary hover:bg-secondary transition-colors"
+        >
+          <ZapIcon className="size-4" />
+          <span className="hidden sm:inline">Pro</span>
+        </Link>
+
+        <SignedIn>
+          <Link
+            href={"/billing"}
+            className="flex items-center gap-1 px-3 py-2 rounded-md text-sm text-muted-foreground hover:text-primary hover:bg-secondary transition-colors"
+          >
+            <Button
+              variant={"outline"}
+              size={"sm"}
+              className="flex items-center gap-2"
+            >
+              <CreditCard className="size-4" />
+              <span className="hidden sm:inline">Billing</span>
+            </Button>
+          </Link>
+        </SignedIn>
+
+        {/* this will be show if user signed in */}
+        <UserButton />
+
+        <SignedIn>
+          <SignOutButton>
+            <Button
+              variant={"outline"}
+              size={"sm"}
+              className="flex items-center gap-2"
+            >
+              <LogOutIcon className="h-4 w-4" />
+              <span className="hidden sm:inline">Log Out</span>
+            </Button>
+          </SignOutButton>
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button variant={"outline"} size={"sm"}>
+              Login
+              <LogIn className="h-4 w-4 ml-2" />
+            </Button>
+          </SignInButton>
+        </SignedOut>
+
+        <SignedOut>
+          <SignUpButton mode="modal">
+            <Button size={"sm"}>Sign Up</Button>
+          </SignUpButton>
+        </SignedOut>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
